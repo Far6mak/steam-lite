f97f76192e90c029d894e6202bb4831d32b6943f
Far6mak <vvffggderg@gmail.com>
2026-06-24 12:50:05 +0300

    обновленный визуал, гифки и исправление мелких недочетов

--- src/components/GenreFilter.jsx
+++ src/components/GenreFilter.jsx
@@ -1,16 +1,28 @@
 import { useGameStore } from "../store/gameStore";
 
 export default function GenreFilter() {
+  const games = useGameStore((state) => state.games);
   const genreFilter = useGameStore((state) => state.genreFilter);
   const setGenreFilter = useGameStore((state) => state.setGenreFilter);
 
-  const genres = ["All", "Action", "RPG", "Shooter", "Strategy", "Indie"];
+  // 🔥 автоматически собираем жанры
+  const genres = [
+    "All",
+    ...Array.from(new Set(games.map((g) => g.genre)))
+  ];
 
   return (
     <select
       value={genreFilter}
       onChange={(e) => setGenreFilter(e.target.value)}
-      style={{ padding: "8px", marginLeft: "10px" }}
+      style={{
+        padding: "8px",
+        marginLeft: "10px",
+        borderRadius: "8px",
+        background: "#0f172a",
+        color: "white",
+        border: "1px solid #1f2937",
+      }}
     >
       {genres.map((g) => (
         <option key={g} value={g}>
